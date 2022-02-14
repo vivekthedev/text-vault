e87a86286a6e91e51e6197c60041ec93881cbe08
Vivek Kumar Singh <vivekhappy786@gmail.com>
2022-02-14 09:31:09 +0530

    ready to deploy

--- core/settings.py
+++ core/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-5r297e@gyl7i&%bfh)g*_txu224l(3zgwy+iz!unv$%@^g99ot"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ["textvault.herokuapp.com/", "127.0.0.1"]
 
